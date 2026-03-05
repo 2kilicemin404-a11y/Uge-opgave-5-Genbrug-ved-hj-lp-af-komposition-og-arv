14923680bb671efefd032977ff249f2dca6641f9
eminkilic <kilicemin404@gmail.com>
2026-03-05 14:25:33 +0100

    .

--- src/src/Room.java
+++ src/src/Room.java
@@ -1,7 +1,31 @@
+
+import java.util.ArrayList;
+
 public class Room {
     String name;
 
-    
+   ArrayList<Lamp> Lamp;
 
+   ArrayList<Window> Window;
 
+   Room(String name){
+       this.name = name;
+       this.Lamp = new ArrayList<>();
+       this.Window = new ArrayList<>();
+   }
+
+   void addLamp(Lamp lamp){
+       Lamp.add(lamp);
+   }
+
+   void addWindow(Window window){
+       Window.add(window);
+
+   }
+int getLampCount(){
+
+}
+int getTotalWatt(){
+
+}
 }
