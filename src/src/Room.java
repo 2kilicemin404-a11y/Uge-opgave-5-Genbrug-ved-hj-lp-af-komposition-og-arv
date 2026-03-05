43986dd645ba150b0b8505cc8ccc961a27536586
eminkilic <kilicemin404@gmail.com>
2026-03-05 14:39:22 +0100

    Clas room, metoder laves

--- src/src/Room.java
+++ src/src/Room.java
@@ -4,28 +4,47 @@ import java.util.ArrayList;
 public class Room {
     String name;
 
-   ArrayList<Lamp> Lamp;
+    ArrayList<Lamp> Lamp;
 
-   ArrayList<Window> Window;
+    ArrayList<Window> Window;
 
-   Room(String name){
-       this.name = name;
-       this.Lamp = new ArrayList<>();
-       this.Window = new ArrayList<>();
-   }
+    Room(String name) {
+        this.name = name;
+        this.Lamp = new ArrayList<>();
+        this.Window = new ArrayList<>();
+    }
 
-   void addLamp(Lamp lamp){
-       Lamp.add(lamp);
-   }
+    void addLamp(Lamp lamp) {
+        Lamp.add(lamp);
+    }
 
-   void addWindow(Window window){
-       Window.add(window);
+    void addWindow(Window window) {
+        Window.add(window);
 
-   }
-int getLampCount(){
+    }
 
+    int getLampCount() {
+        return Lamp.size();
+    }
+
+    int getTotalWatt() {
+        int sum = 0;
+
+        for (Lamp lamp : Lamp) {
+            sum = sum + lamp.watt;
+        }
+        return sum;
+
+    }
+
+    int getTotalWindowArea() {
+        int sum = 0;
+        for (Window window : Window) {
+            sum = sum + window.getAreaCm2();
+
+
+        }
+        return sum;
+    }
 }
-int getTotalWatt(){
 
-}
-}
